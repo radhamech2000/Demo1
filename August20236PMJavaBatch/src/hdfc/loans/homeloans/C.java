7f59da741dbcc9474f21630e7ee3ab8f1817a4a6
radhamech2000 <radhamech2000@gmail.com>
2023-12-23 04:51:43 +0530

    GitDemo

---
+++ August20236PMJavaBatch/src/hdfc/loans/homeloans/C.java
@@ -0,0 +1,23 @@
+package hdfc.loans.homeloans;
+
+import hdfc.loans.carloans.A;
+
+public class C extends A
+{
+	public void m1()
+	{
+		System.out.println("iam overriden m1 from C");
+	}
+	
+	public void m3()
+	{
+		System.out.println("iam m3 from C");
+	}
+
+	public static void main(String[] args) 
+	{		
+		C c = new C();
+		c.m3();
+		c.m1();
+	}
+}
