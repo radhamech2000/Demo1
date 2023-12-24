5ddaa81483ef02277ff37a415c9a3ac30a470b64
radhamech2000 <radhamech2000@gmail.com>
2023-12-24 23:46:31 +0530

    Added GitDemoClass for practice

---
+++ August20236PMJavaBatch/src/hdfc/loans/carloans/GitDemoClass.java
@@ -0,0 +1,11 @@
+package hdfc.loans.carloans;
+
+public class GitDemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Demo for git practice");
+
+	}
+
+}
