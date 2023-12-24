1ebc4e388cb4dbc57e49c0860e2ab2506a29f9ab
radhamech2000 <radhamech2000@gmail.com>
2023-12-25 00:11:08 +0530

    new one

--- August20236PMJavaBatch/src/hdfc/loans/carloans/GitDemoClass.java
+++ August20236PMJavaBatch/src/hdfc/loans/carloans/GitDemoClass.java
@@ -6,7 +6,7 @@ public class GitDemoClass {
 		// TODO Auto-generated method stub
 		System.out.println("Demo for git practice");
 		System.out.println("Added this statement after commit in local repository");
-
+		System.out.println("Final");
 	}
 
 }
