2435eda3bc641d024ccce82c2e87a3f696656a94
radhamech2000 <radhamech2000@gmail.com>
2023-12-24 23:50:48 +0530

    Added another print statement

--- August20236PMJavaBatch/src/hdfc/loans/carloans/GitDemoClass.java
+++ August20236PMJavaBatch/src/hdfc/loans/carloans/GitDemoClass.java
@@ -5,6 +5,7 @@ public class GitDemoClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Demo for git practice");
+		System.out.println("Added this statement after commit in local repository");
 
 	}
 
